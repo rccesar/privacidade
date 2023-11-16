70031be22426a49f9c72fabd95a8411072e318f4
Renato Cesar <renato11cesar@hotmail.com>
2023-11-16 15:09:21 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <text>Política de Privacidade</text>
+          <br /><br />
+          A sua privacidade é importante para nós. É política do Metamoney respeitar a sua privacidade em relação a qualquer informação que possamos coletar no aplicativo Metamoney. <br /><br />
+          Coletamos informações pessoais quando você:<br /><br />
+          - Criar uma conta no aplicativo<br />
+          - Publica um conteúdo no aplicativo <br /><br />
+          As informações pessoais que coletamos incluem: <br /><br />
+          - Seu nome e sobrenome <br />
+          - Seu endereço de e-mail <br />
+          - Seu número de telefone <br />
+          - Sua data de nascimento <br />
+          - Seu sexo <br />
+          Suas preferências pessoais,usamos as informações pessoais que coletamos para: <br /><br />
+          - Fornecer os serviços do aplicativo  <br />
+          - Personalizar a sua experiência no aplicativo <br />
+          - Vincular anúncios <br />
+           Protegemos as suas informações pessoais usando medidas de segurança, incluindo: <br /><br />
+          - Criptografia <br />
+          -Controle de acesso <br />
+          - Monitoramento de segurança <br /><br />
+          Você pode optar por não fornecer algumas informações pessoais, mas isso pode limitar a sua capacidade de usar o aplicativo.
+          Você pode acessar, corrigir ou excluir suas informações pessoais entrando em contato conosco pelo e-mail [metamoneysocial@gmail.com].
+          Compromisso do Usuário
+          O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Metamoney oferece no aplicativo e com caráter enunciativo, mas não limitativo:
+          Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;
+          Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, casa de apostas ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;
+          Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Metamoney, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.
+          Mais informações
+          Esperamos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso aplicativo. <br /><br />
+          Esta política é efetiva a partir de 16 de novembro de 2023.
+
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
